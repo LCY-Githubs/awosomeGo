c01266a67ffcd928b1a6fc236b95821c528db273
lcy <983813520@qq.com>
2023-05-24 22:58:23 +0800

    study go append

--- src/study/template_test.go
+++ src/study/template_test.go
@@ -52,6 +52,7 @@ func TestAnonymousFun(t *testing.T) {
 }
 
 func TestAppendFunc1(t *testing.T) {
+	//第一种用法
 	//func append(slice []Type, elems ...Type) []Type
 	nums := []int{1, 2}
 	fmt.Println(nums)
@@ -59,4 +60,22 @@ func TestAppendFunc1(t *testing.T) {
 	fmt.Println(nums)
 	nums = append(nums, 5, 6, 7)
 	fmt.Println(nums)
+
+	//第二种用法
+	//res := append(slice1, slice2...)
+	//第三种用法
+	//append([]byte(string1),string2...)
+}
+func TestAppendCapBig(t *testing.T) {
+	var nums2 []int
+	nums2 = make([]int, 2, 2)
+	nums2[0] = 0
+	nums2[1] = 1
+	fmt.Println(nums2)
+	fmt.Println(len(nums2))
+	fmt.Println(cap(nums2))
+	nums2 = append(nums2, 3)
+	fmt.Println(nums2)
+	fmt.Println(len(nums2))
+	fmt.Println(cap(nums2))
 }
